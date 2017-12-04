58cb08f9a4da791c6e02235f510ac04f8fe1804b
zzz <18358588190@163.com>
2017-12-04 15:40:23 +0800

    数组学习

--- 数组/数组/ViewController.swift
+++ 数组/数组/ViewController.swift
@@ -13,9 +13,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        demo()
-        demo1()
+//        demo1()
+//        demo3()
+//        demo4()
+        demo5()
     }
-
+    //MARK: - 数组的合并
+    func demo5(){
+        var array = ["张三","消防","搜啊了"] as [Any]
+        let array2 = ["kala","aass",1,180] as [Any]
+        //讲array2的内容合并到array中
+        //注意:要合并数组的两个类型必须一致
+        array += array2
+        print(array)
+    }
+    
+    //MARK: - 数组的容量
+    func demo4(){
+        //定义一个数组,指定类型是 存放 Int 的数组,但是没有初始化
+//        var array : [Int]
+//        //给数组进行初始化
+//        array = [Int]()
+        
+        //以上两句代码可以合并成一句
+        var array = [Int]()
+        for i in 0...8 {
+            array.append(i)
+            print("\(array) \(array.capacity)")
+        }
+    }
+    
+    //MARK: - 数组的增删改
+    func demo3(){
+        var array = ["张三","消防","搜啊了"]
+        //追加元素
+        array.append("哈哈")
+        print(array)
+        
+        //修改,通过下标定位
+        array[0] = "all"
+        print(array)
+        
+        //删除
+        array.remove(at: 3)
+        print(array)
+        
+        //删除全部,并且保留空间
+        array.removeAll(keepingCapacity: true)
+        print("\(array) \(array.capacity)")
+        
+    }
+    
     //MARK: - 数组的遍历
     func demo1(){
         let array = ["经济技术",2,3,4,"说了肯定",CGPoint(x: 10, y: 300)] as [Any]
