7e3305e0578b933ab659207457ffd520352df17f
zzz <18358588190@163.com>
2017-12-05 16:37:39 +0800

    闭包初步学习

--- 闭包的定义/闭包的定义/ViewController.swift
+++ 闭包的定义/闭包的定义/ViewController.swift
@@ -18,8 +18,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //1>最简单的闭包
+        //() -> () 没有参数,没有返回值的函数
+        //如果没有参数,没有返回值,可以省略,连in都一起省略
+        let b1 = {
+            print("hello")
+        }
+        //执行闭包
+        b1()
+        
+        //2> 带参数的闭包
+        //闭包中,参数,返回值,实现代码都是卸载{}中
+        //需要使用一个关键字 'in'
+        //{ 形参列表 -> 返回值类型 in//实现代码 }
+        let b2 = { (x : Int) -> () in
+            print(x)
+        }
+        b2(20)
+        
+        //3>带参数带返回值的比吧
+        let b3 = {
+            (x : Int) -> (Int) in
+            return x + 250
+        }
+        
+        print(b3(100))
     }
-
+    
+    //MARK: - 使用常量记录函数的演练
+    func demo(){
+        //定义一个常量记录 函数
+        let f = sum
+        print(f(10, 20))
+    }
+    
     func sum(x : Int , y : Int) -> Int {
         return x + y
     }
