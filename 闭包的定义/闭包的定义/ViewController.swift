fdfecad94d848d9bff034bb061a177306964a1bb
zzz <18358588190@163.com>
2017-12-05 16:20:23 +0800

    闭包开始学习

---
+++ 闭包的定义/闭包的定义/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  闭包的定义
+//
+//  Created by 周冰烽 on 2017/12/5.
+//  Copyright © 2017年 周冰烽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    /**
+        闭包
+        1.提前准备好的代码
+        2.在需要的时候执行
+        3.可以当做参数传递
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    func sum(x : Int , y : Int) -> Int {
+        return x + y
+    }
+
+
+}
+
