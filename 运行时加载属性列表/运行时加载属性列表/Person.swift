cd946072b558cbe1e2c187453710d5eeac7e60f1
zzz <18358588190@163.com>
2017-12-09 13:44:15 +0800

    用swift运行时获取属性列表,  转换成String类型swift4.0不知道怎么写

---
+++ 运行时加载属性列表/运行时加载属性列表/Person.swift
@@ -0,0 +1,45 @@
+//
+//  Person.swift
+//  运行时加载属性列表
+//
+//  Created by 周冰烽 on 2017/12/9.
+//  Copyright © 2017年 周冰烽. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String?
+    var age: Int = 0
+    var title: String?
+    
+    
+    /// 目标:[使用运行时]获取当前所有属性的数组
+    class func propertyList()->[String] {
+        
+        var count: UInt32 = 0
+        
+        //1.获取 '类' 的属性列表
+        let list = class_copyIvarList(self, &count)
+        
+        print("属性的数量\(count)")
+        
+        //遍历数组
+        for i in 0..<Int(count) {
+            //根据下标获取属性
+            let pty = list?[i]
+            //获取'属性'的名称 C 语言字符串
+            let cName = property_getName(pty!)
+//            print(cName)
+            //转换成String的字符串
+            
+//            let name = String(uft8String:cName)
+//            print(name)
+            
+            //释放C语言的对象
+            free(list)
+        }
+        
+        return []
+    }
+}
---
+++ 运行时加载属性列表/运行时加载属性列表/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  运行时加载属性列表
+//
+//  Created by 周冰烽 on 2017/12/9.
+//  Copyright © 2017年 周冰烽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Person.propertyList())
+    }
+
+}
+
