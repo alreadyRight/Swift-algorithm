27140b36d5e7798b8f49375f2dcca263bf993048
zzz <18358588190@163.com>
2017-12-12 13:24:23 +0800

    懒加载完整写法

--- 懒加载/懒加载/ViewController.swift
+++ 懒加载/懒加载/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     //初始化label,并且分配空间,会提前创建
     //lazy - 懒加载
-    lazy var label: DemoLabel = DemoLabel()
+//    lazy var label: DemoLabel = DemoLabel()
+    // 懒加载本质上是一个'闭包'
+    // 完整写法,日常开发不建议这么写
+    /**
+        {}包装代码
+        ()执行代码
+     
+     日常开发:
+     1.闭包中的只能提示不好
+     2.闭包中如果出现self.还需要注意循环引用
+     */
+    lazy var label = { ()->DemoLabel in
+            let l = DemoLabel()
+        return l
+    }()
     
     private func setupUI(){
         //! -- 解包,为了参与计算,addSubView 用 subViews 数组记录空间,数组中不允许插入nil
